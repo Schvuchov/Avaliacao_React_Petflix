9934732ad603feba7f7358b095864a0ecaee9fc0
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-05-21 13:18:35 -0300

    ajustes para deploy

---
+++ petflix/src/App.jsx
@@ -0,0 +1,19 @@
+import Home from './pages/Home/Home';
+import Pagina1 from './pages/Pagina1/Pagina1';
+import Pagina2 from './pages/Pagina2/Pagina2';
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+            <Route path="/Avaliacao_React_Petflix" element={<Home />} />
+            <Route path="/Avaliacao_React_Petflix/pagina1" element={<Pagina1 />} />
+            <Route path="/Avaliacao_React_Petflix/pagina2" element={<Pagina2 />} />
+        </Routes>
+    </Router>
+  )
+}
+
+export default App;
--- petflix/src/main.jsx
+++ petflix/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './index.css'
 
 import ReactDOM from 'react-dom/client'
-import { Routes } from './routes'
+
+import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Routes />
+    <App />
   </React.StrictMode>,
 )
